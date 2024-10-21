fa4c38e027cd93723525b1edb874c526160a0cc5
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-10-21 15:27:32 +0300

    defines onLogin function and passes it down as a prop to Login Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 // src/components/App.js
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './Signup';
 import Navbar from './Navbar';
@@ -8,6 +8,13 @@ import JobCard from './JobCard';
 
 
 const App = () => {
+
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (userData) => {
+  setUser(userData);
+  console.log('Logged in user:', userData);
+  };
   return (
     <Router>
       <div>
@@ -16,7 +23,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<h1>Welcome to the Laptop Care App</h1>} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
           <Route path="/jobs" element={<JobCard />} />
         </Routes>
       </div>
