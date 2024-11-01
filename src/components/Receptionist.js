13d2fb9384ccd56c9a318b60ed39f9c5236581c4
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-11-01 16:31:02 +0300

    implements device information fetch on jobcard creation

--- src/components/Receptionist.js
+++ src/components/Receptionist.js
@@ -1,10 +1,19 @@
 import React from 'react';
+<<<<<<< HEAD
 import JobCard
+=======
+import JobCard from './JobCard'
+>>>>>>> e286a61 (implements device information fetch on jobcard creation)
 
   from './JobCard';
 const Receptionist = () => {
   return (
+<<<<<<< HEAD
     <div className="min-h-screen flex bg-gradient-to-tl from-yellow-50 to-yellow-100 items-center justify-center">
+=======
+    <div className="">
+  
+>>>>>>> e286a61 (implements device information fetch on jobcard creation)
       <JobCard />
     </div>
   );
