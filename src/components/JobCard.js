969494de642a53e77b33a99a3ac4c24e1a5d6bf3
tomutanyi <tomutanyi@gmail.com>
2025-01-10 17:25:02 +0300

    changed values of hdd to HDD

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -397,8 +397,8 @@ const JobCard = () => {
                   name="hddOrSsd"
                   label="HDD or SSD"
                   options={[
-                    { value: "hdd", label: "HDD" },
-                    { value: "ssd", label: "SSD" },
+                    { value: "HDD", label: "HDD" },
+                    { value: "SSD", label: "SSD" },
                   ]}
                   disabled={!!deviceExists}
                   className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
